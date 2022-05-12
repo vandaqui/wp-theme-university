6183f6fc885fbb7910b9e5b9f1bbfe9130b3e601
Vand Aquino <vandicleydson@gmail.com>
2022-05-11 22:11:06 -0300

    Get stylesheets

--- multiversity-theme/functions.php
+++ multiversity-theme/functions.php
@@ -2,7 +2,8 @@
 
 function university_files(){
     //The first is a nickname, the second is the location of the file
-    wp_enqueue_style('multiversity_main_style', get_stylesheet_uri());
+    wp_enqueue_style('multiversity_main_style', get_themefile_uri('/build/style-index.css'));
+    wp_enqueue_style('multiversity_extra_style', get_themefile_uri('/build/index.css'));
 }
 // Second argument is the name of our function
 add_action('wp_enqueue_scripts','multiversity_files');
