2cb318c22d8bc0ddd780d767cafce69f64e2fe8d
Vand Aquino <vandicleydson@gmail.com>
2022-05-12 21:29:34 -0300

    Add font awesome to functions php

--- multiversity-theme/functions.php
+++ multiversity-theme/functions.php
@@ -2,6 +2,7 @@
 
 function university_files(){
     //The first is a nickname, the second is the location of the file
+    wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('multiversity_main_style', get_themefile_uri('/build/style-index.css'));
     wp_enqueue_style('multiversity_extra_style', get_themefile_uri('/build/index.css'));
 }
