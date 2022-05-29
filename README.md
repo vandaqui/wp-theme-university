f2ce58f0b142c096e5f62b22ffa0051499c13888
Vand Aquino <vandicleydson@gmail.com>
2022-05-29 20:08:34 -0300

    Call images and JS

--- README.md
+++ README.md
@@ -1,6 +1,3 @@
 # wp-theme-university
 A theme for Wordpress
 
-
-The converted theme can be found at LearnWebCode profile:
-https://github.com/LearnWebCode/university-static
--- functions.php
+++ functions.php
@@ -1,11 +1,10 @@
 <?php
 
 function multiversity_files(){
-    //The first is a nickname, the second is the location of the file
+    wp_enqueue_script('main-multiversity-js', get_theme_file_uri('/build/index.js'), array('jquery'), '1.0', true);
     wp_enqueue_style( 'google-fonts', '//fonts.googleapis.com/css?family=Roboto+Condensed:300,300i,400,400i,700,700i|Roboto:100,300,400,400i,700,700i');
     wp_enqueue_style('font-awesome', '//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css');
     wp_enqueue_style('multiversity_main_style', get_theme_file_uri('/build/style-index.css'));
     wp_enqueue_style('multiversity_extra_style', get_theme_file_uri('/build/index.css'));
 }
-// Second argument is the name of our function
 add_action('wp_enqueue_scripts','multiversity_files');
